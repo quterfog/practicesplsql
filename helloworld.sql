39ec7471dd246f0780b0e87a95f1e6354a5be881
Alexandr <ns-95@yandex.ru>
2019-03-30 16:08:44 +0300

    add new command

--- helloworld.sql
+++ helloworld.sql
@@ -2,9 +2,11 @@ SET SERVEROUTPUT ON
 DECLARE
   v_today DATE := SYSDATE;
   v_tomorrow v_today%TYPE;
+  v_name VARCHAR2 := 'Alexandr';
 BEGIN
   v_tomorrow := v_today + 1;
   dbms_output.put_line('Hello World');
+  dbms_output.put_line('MY NAME IS '||v_name);
   dbms_output.put_line('TODAY IS '||v_today);
   dbms_output.put_line('TOMORROW IS '||v_tomorrow);
 END;
